ade626b7b3d61895a1fe59c6782f873c56c182e0
ankit <sahuankit.murhu@gmail.com>
2025-03-30 07:14:44 +0530

    Base

--- app/index.tsx
+++ app/index.tsx
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
     gap: 10,
   },
   title: {
-    fontSize: 50,
+    fontSize: 30,
     fontWeight: 'bold',
     color: colors['zinc-200'],
     fontVariant: ['small-caps'],
